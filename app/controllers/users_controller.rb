49a3684cfdf0c03a660714cbb80eddf9dc48ed05
gregspagnolo <dwdrummer@gmail.com>
2016-01-26 21:46:29 -0600

    set up authentication methods and views for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
 
+	before_action :set_user, only: [:edit, :update, :show]
+
 	def index
 		@users = User.paginate(page: params[:page], per_page: 3)
 	end
@@ -20,11 +22,10 @@ def create
 end
 
 def edit
-  @user = User.find(params[:id])
+ 
 end
 
 def update
-@user = User.find(params[:id])
  if @user.update(user_params)
  	flash[:success] = "Your account was updated successfully"
  	redirect_to articles_path
@@ -36,8 +37,11 @@ def update
 end
 
 def show
-	@user = User.find(params[:id])
 	@user_articles = @user.articles.paginate(page: params[:page], per_page: 3)
+end
+
+def set_user
+	 @user = User.find(params[:id])
 end
 
 private
